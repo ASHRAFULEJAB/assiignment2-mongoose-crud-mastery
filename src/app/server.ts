1ad0e9bd4d2eab9e5602f8fb23144352e5937a8a
Ashraful Islam <19103189@iubat.edu>
2023-11-23 18:32:36 +0600

    inital setup done

---
+++ src/app/server.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+// getting-started.js
+import mongoose from "mongoose";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(` Mongoose Crud Mastery listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
